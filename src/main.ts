bfd1fb9e4705324fbd342ea487605dea4b04bca6
Piotr Tabor <ptab@google.com>
2022-12-11 12:13:44 +0000

    Changing the dependency

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
-import {workflowArtifactsPullRequestCommentAction} from '@tonyhallett/github-actions'
+import {workflowArtifactsPullRequestCommentAction} from '@ptabor/github-actions'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+core.info('Starting')
+core.info(JSON.stringify(github.context.payload))
+core.info('Really Starting...')
 workflowArtifactsPullRequestCommentAction()
